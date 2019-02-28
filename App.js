9f6c449d17bdcfc587a1c7a68b719db3e225ce6a
Beautifulife <davidkim1018@gmail.com>
2019-02-28 14:07:20 +0900

    Make base ui - frame, card

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, TextInput, Platform } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Beautiful To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder="New To Do" />
+        </View>
       </View>
     );
   }
 }
 
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  title: {
+    marginTop: height * 0.1,
+    marginBottom: height * 0.05,
+    fontSize: 30,
+    fontWeight: '400',
+    color: 'white'
+  },
+  card: {
+    flex: 1,
+    width: width - 50,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+
+  }
 });
